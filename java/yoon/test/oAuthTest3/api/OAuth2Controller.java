bbba53b6f6f0b4f4d7cc5dde50cb2e602a4092e8
YoonSeungKwon <113295771+YoonSeungKwon@users.noreply.github.com>
2024-05-26 20:30:01 +0900

    Update OAuth2Controller.java

--- java/yoon/test/oAuthTest3/api/OAuth2Controller.java
+++ java/yoon/test/oAuthTest3/api/OAuth2Controller.java
@@ -1,35 +1,58 @@
-package yoon.test.oAuthTest3.api;
+package yoon.docker.memberService.controller;
 
+import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-import yoon.test.oAuthTest3.security.jwt.JwtProvider;
-import yoon.test.oAuthTest3.service.OAuth2CredentialService;
-import yoon.test.oAuthTest3.vo.request.OAuthRequest;
-import yoon.test.oAuthTest3.vo.response.OAuthResponse;
+import yoon.docker.memberService.dto.response.MemberResponse;
+import yoon.docker.memberService.service.OAuthService;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/api/v1/oauth2")
-public class OAuth2Controller {
+@RequestMapping("/api/v1/oauth")
+public class OAuthController {
 
-    private final OAuth2CredentialService oAuth2CredentialService;
-    private final JwtProvider jwtProvider;
-    @PostMapping("/google/callback")
-    public ResponseEntity<?> googleCallBack(@RequestBody OAuthRequest oAuthRequest){
+    private final OAuthService oAuthService;
 
-        if("".equals(oAuthRequest.getEmail()) && "".equals(oAuthRequest.getName()) && "".equals(oAuthRequest.getProvider()))
-            return ResponseEntity.badRequest().body("No Item");
 
-        OAuthResponse result = oAuth2CredentialService.save(oAuthRequest);
+    @GetMapping("/callback/kakao")
+    public ResponseEntity<MemberResponse> kakaoAuth(@RequestParam("code") String code, HttpServletResponse response){
 
-        String token = jwtProvider.createAccessToken(oAuthRequest.getEmail(), oAuthRequest.getName());
+        System.out.println(code);
 
-        return ResponseEntity.ok(token);
+        MemberResponse result = oAuthService.kakaoLogin(code, response);
+
+        return new ResponseEntity<>(result, HttpStatus.OK);
     }
 
+    @GetMapping("/callback/naver")
+    public ResponseEntity<MemberResponse> naverAuth(@RequestParam("code") String code, HttpServletResponse response){
+
+        MemberResponse result = oAuthService.naverLogin(code, response);
+
+        return new ResponseEntity<>(result, HttpStatus.OK);
+    }
+
+    @GetMapping("/callback/google")
+    public ResponseEntity<MemberResponse> googleAuth(@RequestParam("code") String code, HttpServletResponse response){
+
+        MemberResponse result = oAuthService.googleLogin(code, response);
+
+        return new ResponseEntity<>(result, HttpStatus.OK);
+    }
+
+    @GetMapping("/login/kakao")
+    public ResponseEntity<MemberResponse> kakaoAccessLogin(@RequestParam("access_token") String token, HttpServletResponse response){
+
+        System.out.println(token);
+
+        MemberResponse result = oAuthService.kakaoLogin(token, response);
+
+        return new ResponseEntity<>(result, HttpStatus.OK);
+    }
 
 }
